9e19fc3992ac04bfe16beaa6a7e079003609dad9
ibodi <b.szczerbak@gmail.com>
2019-05-15 00:28:29 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Bohdan Shcherbak on 15/05/2019.
+//  Copyright © 2019 Bohdan Shcherbak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
